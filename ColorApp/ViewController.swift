01432e209fc2e5259832c5ca6d780fe75e398439
Siddharth Kurnal <49497542+skurnal2@users.noreply.github.com>
2020-01-22 17:02:07 -0700

    Initial Commit

---
+++ ColorApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColorApp
+//
+//  Created by Siddharth Kurnal on 1/22/20.
+//  Copyright © 2020 Siddharth Kurnal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
