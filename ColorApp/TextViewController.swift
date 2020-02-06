053758d99ac06090ec952cb94b36698835ec870f
Siddharth Kurnal <49497542+skurnal2@users.noreply.github.com>
2020-02-05 21:53:02 -0700

    Text Add, Size functionality
    Next Idea - Give a button on top right to complete the text change and reflect that on the previous page. Complete also takes back to the previous page. But, before complete performs function it should give a prompt that "Are you sure you want to change the text - Yes/No". Once the this functionality is done, then i'll implement change color on font, its border on the main page. and also change border size on the text page.

--- ColorApp/TextViewController.swift
+++ ColorApp/TextViewController.swift
@@ -15,41 +15,36 @@ class TextViewController: UIViewController {
 
     @IBOutlet weak var textToSet: UITextField!
     
+    @IBOutlet weak var sizeSliderValue: UISlider!
     
+    @IBAction func sizeSlider(_ sender: Any) {
+        setTextBox(textString: textToSet.text!, fontSize: Float(sizeSliderValue.value))
+    }
     @IBOutlet weak var textOutput: UILabel!
     
     @IBAction func changeText(_ sender: UIButton) {
-        setTextBox(textOutput.text!)
+        setTextBox(textString: textToSet.text!, fontSize: Float(sizeSliderValue.value))
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let textAttributes = [
-            NSAttributedString.Key.strokeColor : UIColor.black,
-            NSAttributedString.Key.foregroundColor : UIColor.white,
-            NSAttributedString.Key.strokeWidth : -4.0,
-            NSAttributedString.Key.font : UIFont.boldSystemFont(ofSize: 60)]
-            as [NSAttributedString.Key : Any]
-        textOutput.backgroundColor = backColor
-        
-        
-        textOutput.attributedText = NSMutableAttributedString(string: "SAMPLE", attributes: textAttributes)
+        setTextBox(textString: "SAMPLE", fontSize: Float(sizeSliderValue.value))
         
         // Do any additional setup after loading the view.
     }
     
-    func setTextBox(_ text : String) {
+    func setTextBox(textString : String, fontSize : Float) {
         let textAttributes = [
             NSAttributedString.Key.strokeColor : UIColor.black,
             NSAttributedString.Key.foregroundColor : UIColor.white,
             NSAttributedString.Key.strokeWidth : -4.0,
-            NSAttributedString.Key.font : UIFont.boldSystemFont(ofSize: 60)]
+            NSAttributedString.Key.font : UIFont.boldSystemFont(ofSize: CGFloat(Int(fontSize)))]
             as [NSAttributedString.Key : Any]
         textOutput.backgroundColor = backColor
         
         
-        textOutput.attributedText = NSMutableAttributedString(string: text, attributes: textAttributes)
+        textOutput.attributedText = NSMutableAttributedString(string: textString, attributes: textAttributes)
     }
     
 
