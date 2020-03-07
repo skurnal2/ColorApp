d2d523871123ee6fba1d68740f28337fca766da6
Siddharth Kurnal <49497542+skurnal2@users.noreply.github.com>
2020-03-06 17:53:07 -0700

    Added Tab Bar and New VC for Picker

---
+++ ColorApp/PickerViewController.swift
@@ -0,0 +1,43 @@
+//
+//  PickerViewController.swift
+//  ColorApp
+//
+//  Created by Siddharth Kurnal on 3/6/20.
+//  Copyright © 2020 Siddharth Kurnal. All rights reserved.
+//
+
+import UIKit
+
+class PickerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 255
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(row+1)
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
